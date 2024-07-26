390901135ae9974165c08ad3a67bda144e91f273
robrub <roberto@refactormi.com>
2024-07-26 12:10:35 +0200

    Aggiunti FIX

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+//FIXME: va in errore con undefined e date o altri tipi di oggetti.
 const somma = (a, b) => {
+  // Deve dare errore in caso di:
+  // stringhe, date, undefined, oggetti, null
   if (typeof a === "string" || typeof b === "string") {
     console.error("Errore: uno dei valori è una stringa.");
     return 0;
@@ -10,9 +13,11 @@ const moltiplica = (...args) => {
   if (args.length < 2) {
     throw new Error("La funzione moltiplica richiede almeno due argomenti");
   }
+  //FIXME: estrarre la funzione di accumulazione per averla non inline anonima
   return args.reduce((acc, curr) => acc * curr, 1);
 };
 
-console.log(somma(5, 3));
+console.log(somma(5, 11));
 
-console.log(moltiplica(2, 3));
+//FIXME: gestire il throw dell'errore
+console.log(moltiplica(2));
